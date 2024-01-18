f270ff9d353878da06dad799952806c8b43538aa
ComeSarazin <sasacoco@orange.fr>
2024-01-18 15:54:32 +0100

    j ai fini le truc qui bouge tout seul

--- back-php/migrations/Version20240118134522.php
+++ back-php/migrations/Version20240118134522.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240117183031 extends AbstractMigration
+final class Version20240118134522 extends AbstractMigration
 {
     public function getDescription(): string
     {
---
+++ back-php/src/Controller/ProfilUserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/profil', name: 'app_')]
+class ProfilUserController extends AbstractController
+{
+    #[Route('/', name: 'user')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $userRepository = $entityManager->getRepository(User::class);
+        $user = $userRepository->findAll();
+
+        return $this->render('profil_user/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
