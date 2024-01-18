4e3d2983135f6e6981bbc38c7ecfe76924f2cac7
Didixe <Adeline.gilet@gmail.com>
2024-01-18 22:27:45 +0100

    moisissure, platre et autres

--- back-php/src/Entity/User.php
+++ back-php/src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
+//#[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
